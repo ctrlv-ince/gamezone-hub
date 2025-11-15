45eb82eaa9f9cce711bbd8539143e8b6aa6c527f
verol14 <nyseam14@gmail.com>
2025-11-15 12:39:28 +0800

    fixed api call

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const API_URL = `${import.meta.env.VITE_API_BASE_URL}/auth`;
 
 const register = (userData) => {
-  return axios.post(API_URL + 'register', userData);
+  return axios.post(`${API_URL}/register`, userData);
 };
 
 const login = (userData) => {
-  return axios.post(API_URL + 'login', userData);
+  return axios.post(`${API_URL}/login`, userData);
 };
 
 export default {
